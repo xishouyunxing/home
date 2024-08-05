db7f38861549116389a8a921bb59fed936181ac4
xishouyunxing <135615287+xishouyunxing@users.noreply.github.com>
2024-08-05 18:27:08 +0800

    Update Loading.vue

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -6,7 +6,7 @@
         <span class="name">
           {{ siteName }}
         </span>
-        <span class="tip"> 加载中 </span>
+        <span class="tip"> 歪，开门！ </span>
       </div>
     </div>
     <div class="loader-section section-left" />
